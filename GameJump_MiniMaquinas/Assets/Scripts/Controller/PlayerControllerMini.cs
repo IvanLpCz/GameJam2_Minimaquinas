00de968dff6a7480a9cfd584b9dd028c30333fdb
IvanLpCz <ivanlopezcozar@gmail.com>
2021-10-05 19:28:34 +0200

    Trabajo en clase
    Update en el playercontrollermini

--- GameJump_MiniMaquinas/Assets/Scripts/Controller/PlayerControllerMini.cs
+++ GameJump_MiniMaquinas/Assets/Scripts/Controller/PlayerControllerMini.cs
@@ -8,6 +8,8 @@ namespace Controller
 {
     public class PlayerControllerMini : MonoBehaviour
     {
+        public GameObject lider;
+        private Vector3 offset;
         private void Update()
         {
             if (Input.GetMouseButtonDown(0))
@@ -21,9 +23,12 @@ namespace Controller
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             bool hashit = Physics.Raycast(ray, out hit);
-            if(hashit)
+            if (hashit)
             {
                 GetComponent<NavMeshAgent>().destination = hit.point;
+
+                //Vector3 offsetN = ray.GetPoint(hit);
+                //offset = lider.transform.position - offsetN;
             }
         }
     }
