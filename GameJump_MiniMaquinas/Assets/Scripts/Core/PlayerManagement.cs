a8794bff8baafa7b7d73f048e5015c82b5f1c26d
IvanLpCz <ivanlopezcozar@gmail.com>
2021-10-08 11:48:13 +0200

    Cámaras Terreno
    El sistema de cámaras esta implementado
    
    El terreno tiene sus texturas disponibles

--- GameJump_MiniMaquinas/Assets/Scripts/Core/PlayerManagement.cs
+++ GameJump_MiniMaquinas/Assets/Scripts/Core/PlayerManagement.cs
@@ -6,7 +6,7 @@ namespace Core
 {
     public class PlayerManagement : MonoBehaviour
     {
-        public GameObject playerBig, playerMin;
+        public GameObject playerBig, playerMin, cameraBig, cameraMin;
 
 
         private void Update()
@@ -18,16 +18,14 @@ namespace Core
         }
         private void SwaptCharacterToMini()
         {
-            //playerMin.SetActive(true);
-            //playerMin.transform.position = playerBig.transform.position;
-            //playerMin.transform.rotation = playerBig.transform.rotation;
-            //playerBig.SetActive(false);
             if (playerBig.activeSelf)
             {
                 playerBig.SetActive(false);
                 playerMin.transform.position = playerBig.transform.position;
                 playerMin.transform.rotation = playerBig.transform.rotation;
                 playerMin.SetActive(true);
+                cameraBig.SetActive(false);
+                cameraMin.SetActive(true);
             }
             else if (playerMin.activeSelf)
             {
@@ -35,6 +33,8 @@ namespace Core
                 playerBig.transform.position = playerMin.transform.position;
                 playerBig.transform.rotation = playerMin.transform.rotation;               
                 playerBig.SetActive(true);
+                cameraMin.SetActive(false);
+                cameraBig.SetActive(true);
             }
 
         }
